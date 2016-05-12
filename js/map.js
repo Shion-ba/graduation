47f6e365fecd689c3e74f4a5a03610c258ac6b73
kabotya <kiyono.shima@gmail.com>
2016-05-12 17:25:25 +0900

    update spotmessage

--- js/map.js
+++ js/map.js
@@ -1,94 +1,104 @@
 spotArray = [ "平和記念公園", "斎場御嶽", "中村家住宅", "勝連城跡", 
 	"KING TACOS", "美ら海水族館", "パイナップルパーク", "むら咲むら", "玉陵", "首里城"];
 
-spotContent = {
+spotContents = {
 	"平和記念公園": {
 		img: "hoge",
-		text: "「戦争の悲惨さ、命の尊さを学べる場所だなぁ。なんだか涙が出てくるよ…。」",
+		msg: "「戦争の悲惨さ、命の尊さを学べる場所だなぁ。なんだか涙が出てくるよ…。」",
 		action: ""
 	},
 	"斎場御嶽": {
 		img: "hoge",
-		text: "「昔は、男子禁制の場所で王様さえ女装しないと入れなかったんだって！神聖な場所なんだね〜。」",
+		msg: "「昔は、男子禁制の場所で王様さえ女装しないと入れなかったんだって！神聖な場所なんだね〜。」",
 		action: 4,
 	},
 	"中村家住宅": {
 		img: "hoge",
-		text: "「戦前の沖縄の住居建築の特色を全て備えているんだって！なんだか時間もゆっくり流れている気がする〜」",
+		msg: "「戦前の沖縄の住居建築の特色を全て備えているんだって！なんだか時間もゆっくり流れている気がする〜」",
 		action: ""
 	},
 	"勝連城跡": {
 		img: "hoge",
-		text: "「沖縄のお城って首里城だけじゃなくて、実は300以上も[グスク跡]があるといわれているんだって！城＝グスクは、グスク＝城ではないらしい…奥深いなあ！」",
+		msg: "「沖縄のお城って首里城だけじゃなくて、実は300以上も[グスク跡]があるといわれているんだって！城＝グスクは、グスク＝城ではないらしい…奥深いなあ！」",
 		action: ""
 	},
 	"KING TACOS": {
 		img: "hoge",
-		text: "「こ、これは…結構ボリューミーなタコライス！定番はチーズ野菜！ほかにもハンバーガーや、[チキンバラバラ]なんてメニューもある。気になるなぁ。』",
+		msg: "「こ、これは…結構ボリューミーなタコライス！定番はチーズ野菜！ほかにもハンバーガーや、[チキンバラバラ]なんてメニューもある。気になるなぁ。』",
 		action: ""
 	},
 	"美ら海水族館": {
 		img: "hoge",
-		text: "「国内最大級のアクリル水槽！ジンベエザメは圧巻の迫力！ぬいぐるみ、買って帰ろうかなぁ…」",
+		msg: "「国内最大級のアクリル水槽！ジンベエザメは圧巻の迫力！ぬいぐるみ、買って帰ろうかなぁ…」",
 		action: ""
 	},
 	"パイナップルパーク": {
 		img: "hoge",
-		text: "「パイナップルって木に実るのかと思っていたけど、下から生えてくるんだね！手でちぎって食べるスナックパインなんてあるんだ！甘酸っぱくておいしい！」",
+		msg: "「パイナップルって木に実るのかと思っていたけど、下から生えてくるんだね！手でちぎって食べるスナックパインなんてあるんだ！甘酸っぱくておいしい！」",
 		action: ""
 	},
 	"むら咲むら": {
 		img: "hoge",
-		text: "「沖縄の文化や伝統を体験できる場所！世界遺産や、焼物工房まであるんだ〜。全部回ってみようかな！」",
+		msg: "「沖縄の文化や伝統を体験できる場所！世界遺産や、焼物工房まであるんだ〜。全部回ってみようかな！」",
 		action: ""
 	},
 	"玉陵": {
 		img: "hoge",
-		text: "「大きいなぁ…！琉球王国の王様のお墓なのか…和製のピラミッドみたいな感じかな？」",
+		msg: "「大きいなぁ…！琉球王国の王様のお墓なのか…和製のピラミッドみたいな感じかな？」",
 		action: ""
 	},
 	"首里城": {
 		img: "hoge",
-		text: "「立派なお城だなぁ！沖縄らしい真っ赤な外観と中国風な作りが面白い！」",
+		msg: "「立派なお城だなぁ！沖縄らしい真っ赤な外観と中国風な作りが面白い！」",
 		action: ""
 	}
 };
 
 function spot ( mass ) {
-	console.log('spotだお');
+
+	var spot;
 
 	switch ( mass ) {
 		case 45:
-			console.log( spotArray[ 0 ] + "だお" );
+			spot = spotArray[ 0 ];
 			break;
 		case 40:
-			console.log( spotArray[ 1 ] + "だお" );
+			spot = spotArray[ 1 ];
 			break;
 		case 35:
-			console.log( spotArray[ 2 ] + "だお" );
+			spot = spotArray[ 2 ];
 			break;
 		case 30:
-			console.log( spotArray[ 3 ] + "だお" );
+			spot = spotArray[ 3 ];
 			break;
 		case 25:
-			console.log( spotArray[ 4 ] + "だお" );
+			spot = spotArray[ 4 ];
 			break;
 		case 20:
-			console.log( spotArray[ 5 ] + "だお" );
+			spot = spotArray[ 5 ];
 			break;
 		case 15:
-			console.log( spotArray[ 6 ] + "だお" );
+			spot = spotArray[ 6 ];
 			break;
 		case 10:
-			console.log( spotArray[ 7 ] + "だお" );
+			spot = spotArray[ 7 ];
 			break;
 		case 5:
-			console.log( spotArray[ 8 ] + "だお" );
+			spot = spotArray[ 8 ];
 			break;
 		case 2:
-			console.log( spotArray[ 9 ] + "だお" );
+			spot = spotArray[ 9 ];
 			break;
 	}
+
+	displaySpotModal(spot);
+}
+
+function displaySpotModal(spot) {
+	$('.modal_kabotya').css('display','block');
+  $('.modal_kabotya').load("templates/kabotya_spot.html", function() {
+		$('.spot_contents_text').text(spotContents[spot].msg);
+  });
 }
 
 function action ( mass ) {

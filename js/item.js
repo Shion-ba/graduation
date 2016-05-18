ed007bd1c1cfd18f78147a0a691ab693ea0ea2ae
kabotya <kiyono.shima@gmail.com>
2016-05-18 16:19:59 +0900

    Update result
    まわったスポットの数取得したアイテムの種類同じく小エレクションの種類が出てくるようになった

--- js/item.js
+++ js/item.js
@@ -1,3 +1,6 @@
+var itemCount = 0;
+var collectionCount = 0;
+
 items = ["images/items/item-anda.png",
 		"images/items/item-awa.png",
 		"images/items/item-chin.png",
@@ -15,15 +18,22 @@ collections = [
 		"images/collection/item-jin.png",
 		];
 
-function getItem(num){
-	$("#item_"+ num )
-	.attr("src",items[num-1]);
-	$("#item_"+ num).css("pointer-events","auto");
+function getItem( num, visited ){
+	$( "#item_" + num ).attr( "src", items[ num - 1 ] );
+	$( "#item_" + num ).css( "pointer-events", "auto" );
+
+	if ( !visited ) {
+		itemCount ++;
+		visited = true;
+	};
 }
 
-function getCollection(num){
-	$("#collection_"+ num )
-	.attr("src",collections[num-1]);
-	$("#collection_"+ num).css("pointer-events","auto");
-}
+function getCollection( num, visited ){
+	$( "#collection_" + num ).attr( "src", collections[ num - 1 ] );
+	$( "#collection_"+ num ).css( "pointer-events", "auto" );
 
+	if ( !visited ) {
+		collectionCount ++;
+		visited = true;
+	};
+}
